36db173c4ed71ddb14e2609c3e69c12e5148329e
Mohab <mr.2m0055@gmail.com>
2024-12-13 14:59:51 +0200

    First pack of spaghitte

---
+++ src.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+
+class Wallet:
+    # needing singlton?
+    # adding db changes
+    # reading from db at __init__??
+    # adding logger
+    # treating currency
+
+    def __init__(self, amount: int | float):
+        self.amount = amount
+    
+    @property
+    def amount(self):
+        return self.__amount
+    
+    @amount.setter
+    def amount (self, amount: int | float):
+        self.__amount = amount
+    
+    def transaction(self, amount: int | float):
+        self.amount += amount
+    # def deposit(self, amount: int | float):
+    #     self.amount += amount
+    
+    # def withdraw(self, amount: int | float):
+    #     self.amount -= amount
+
+
+class Transaction:
+    def __init__(self, amount: int | float, name: str, category: str | None = None):
+        ...
+    ...
+
+
+class Deposit(Transaction):
+    def __init__(self, amount: int | float, name: str = "deposit", category: str | None = None):
+        super().__init__(name, amount, category)
+
+
+class Withdraw(Transaction):
+    def __init__(self, amount: int | float, name: str = "deposit", category: str | None = None):
+        super().__init__(name, amount, category)
+
+
+class Periodic(Transaction):
+    def __init__(self, name: str, amount: int | float, period: datetime, category: str | None = None):
+        super().__init__(name, amount, category)
+        self.data = period
+
+
+class Expenses(Periodic):
+    def __init__(self, name, amount, period, category = None):
+        super().__init__(name, amount, period, category)
+
+
+class Income(Periodic):
+    def __init__(self, name, amount, period, category = None):
+        super().__init__(name, amount, period, category)
+
+ 
