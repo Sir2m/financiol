7e4aac662626d1ad9dcc3830f6118be03ef3e7bd
Hossam Eldin Mahmod <hossammahmod555@gmail.com>
2024-12-21 18:46:43 +0200

    adding calc_interest function

--- CalcOperations.py
+++ CalcOperations.py
@@ -15,4 +15,23 @@ class CalcOperations:
         else:
             final = value - change
         
-        return final
+        return final 
+
+    @staticmethod
+    def calc_interest(principal: int | float, rate: int | float, periods: int, compound: bool | None = True, total_amount: bool | None = True) -> int | float:
+        
+        if rate > 1:
+            rate /= 100
+
+        if compound:
+            # A = P * (1 + r)^(t)
+            total = principal * ((1 + rate) ** periods)
+        else:
+            # A = P * (1 + rt)
+            total = principal * (1 + rate * periods)
+
+        if not total_amount:
+            return total - principal  # Return only the interest
+        else:
+            return total      
+    
