ac0d88eed6d9b7c97c2330a4eddb483bb4c1ba33
Mohab <mr.2m0055@gmail.com>
2024-12-22 09:46:22 +0200

    last commit reviewed!

--- CalcOperations.py
+++ CalcOperations.py
@@ -1,10 +1,8 @@
 class CalcOperations:
 
     @staticmethod
-    def partitioning(value: int | float, percent: int | float, slope: bool | None = True, full: bool | None = True) -> int | float:
-        if percent > 1:
-            percent /= 100
-        
+    def partitioning(value: int | float, percent: int | float, slope: bool | None = True, full: bool | None = True) -> int | float:        
+        percent /= 100
         change = value * percent
         
         if not full:
@@ -18,20 +16,13 @@ class CalcOperations:
         return final 
 
     @staticmethod
-    def calc_interest(principal: int | float, rate: int | float, periods: int, compound: bool | None = True, total_amount: bool | None = True) -> int | float:
-        
-        if rate > 1:
-            rate /= 100
+    def calc_interest(principal: int | float, rate: int | float, periods: int, installments: bool | None = True) -> int | float:
+        rate /= 100
 
-        if compound:
-            # A = P * (1 + r)^(t)
-            total = principal * ((1 + rate) ** periods)
+        if installments:
+            result = principal * ((rate * ((1+rate) ** periods))/(((1+rate) ** periods) - 1))
         else:
-            # A = P * (1 + rt)
-            total = principal * (1 + rate * periods)
+            result = principal * ((1 + rate) ** periods)
 
-        if not total_amount:
-            return total - principal  # Return only the interest
-        else:
-            return total      
+        return result
     
