72de9c126635a6af194b6fc81bf5e1bde19705f0
Mohab <mr.2m0055@gmail.com>
2024-12-26 20:38:46 +0200

    Creating the accoutns db class

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import sqlite3
 import requests
 from enum import Enum
+from werkzeug.security import check_password_hash, generate_password_hash
 
 class DB_enums(Enum):
     ADD = 1
@@ -221,3 +222,56 @@ class DB_connection:
 
     def flow_hist(self):
         ...
+    
+
+class DB_accounts:
+    PATH = "accounts.db"
+    __instance = None
+
+    def __init__(self):
+        self.__db = sqlite3.connect(self.PATH)
+
+
+    def __new__(cls):
+        if cls.__instance is None: 
+            cls.__instance = super(DB_connection, cls).__new__(cls)
+        return cls.__instance
+
+
+    def __del__(self):
+        self.__db.commit()
+        self.__db.close()
+
+    
+    # just made for the tests
+    @classmethod
+    def config(cls, path: str):
+        # a way to check that it is genuine path?
+        cls.PATH = path
+
+
+    def create(self):
+        self.__db.execute("""CREATE TABLE accounts (
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+            userID INTEGER NOT NULL UNIQUE PRIMARY KEY
+            primary_currency NOT NULL
+            );""")
+    
+    def uniquness(self, username):
+        unique = self.__db.execute("SELECT * FROM accounts WHERE username = ?", username)
+        
+        if len(unique) != 0:
+            return False
+        
+        return True
+
+    def add_account(self, username: str, password: str, currency: str):
+        self.__db.execute("INSERT INTO accounts (username, password, currency) VALUES (?, ?, ?)", (username, generate_password_hash(password), currency))
+
+    def login(self, username: str, password: str):
+        y = self.__db.execute("SELECT password, userID FROM accounts WHERE username = ?", (username))
+        if check_password_hash(y[0]["password"], password):
+            return y[0]["userID"]
+        else:
+            raise ValueError("Wrong Password!")
--- test_db.py
+++ test_db.py
@@ -4,22 +4,24 @@ import os
 
 
 @pytest.fixture
-def test_db():
+def db():
     name = "test.db"
     DB_connection.config(name)
     return
 
 
-def test_singleton(test_db):
+def test_singleton(db):
     assert DB_connection() is DB_connection()
+
 
 def test_standard_queries():
     # as long as it works, it is almost right, checking rightness is in later tests
-      x = DB_connection()
-      x.create()
-      x.get_history()
-      x.get_flow()
-      x.get_wallet()
+    x = DB_connection()
+    x.create()
+    x.get_history()
+    x.get_flow()
+    x.get_wallet()
+
 
 DB = DB_connection()
 def test_wallet_add():
@@ -31,20 +33,24 @@ def test_wallet_add():
     assert wallet['USD'] == 500
     assert wallet['SAR'] == 800
 
+
 def test_wallet_edit_add():
     DB.edit_wallet("JPY",500,DB_enums.ADD)
     wallet = DB.get_wallet()
     assert wallet['JPY'] == 1500
+
 
 def test_wallet_edit_sub():
     DB.edit_wallet("USD",200,DB_enums.SUB)
     wallet = DB.get_wallet()
     assert wallet['USD'] == 300
 
+
 def test_wallet_edit_set():
     DB.edit_wallet("SAR",1000,set=True)
     wallet = DB.get_wallet()
     assert wallet['SAR'] == 1000
+
 
 def test_wallet_get():
     wallet = dict(DB.get_wallet())
