36936b195a4fe7368dd4c178df0e5d9c6e61626b
Mohab <mr.2m0055@gmail.com>
2024-12-27 19:55:46 +0200

    converting the research-based functions into system-based class

--- testingjson.py
+++ testingjson.py
@@ -1,28 +1,77 @@
 import json
 
-# Load user preferences from JSON file
-def load_preferences(filepath, user_id):
-    with open(filepath, 'r') as file:
-        data = json.load(file)
-        if user_id not in data['users']:
-            # Initialize default preferences for new user
-            data['users'][user_id] = {
-                "remember_me": False,
-                "theme": "light",
-                "currency": "USD"
+class Meta_data:
+    __data = None
+    PATH = "preference.json"
+
+    def __init__(self):
+        def load_data():
+            with open(self.PATH, 'r') as file:
+                self.__data = json.load(file)
+
+        try:
+            load_data()
+        except:
+            with open(self.PATH, 'w') as file:
+                x = {
+                        "theme": "light",
+                        "currency": "",
+                        "user_id": "",
+                        "other_users": {}
+                    }
+                json.dump(x, file, indent=3)
+            load_data()
+        self.__theme = self.__data['theme']
+        self.__currency = self.__data['currency']
+        self.__id = self.__data['user_id']
+        print(self.__theme)
+
+
+    def __new__(cls):
+        if cls.__data is None: 
+            cls.__data = super(Meta_data, cls).__new__(cls)
+        return cls.__data
+
+
+    def __del__(self):
+        self.__data['theme'] = self.__theme
+        self.__data['currency'] = self.__currency
+        self.__data['user_id'] = self.__id
+        with open(self.PATH, 'w') as file:
+            json.dump(self.__data, file, indent=4)
+
+
+    def theme_change(self):
+        x = {
+            "light": "dark",
+            "dark": "light"
+        }
+        self.__theme = x[self.__theme]
+
+
+    def set_base_currency(self, currency:str):
+        self.__currency = currency
+
+
+    def change_account(self, remember, id):
+        if remember:
+            self.__data['other_users'][self.__id] = {
+                "theme": self.__theme,
+                "currency": self.__currency,
+            } 
+        new = self.__data['other_users'][id]
+        self.__id = id
+        self.__theme = new["theme"]
+        self.__currency = new["currency"]
+
+
+    def set_new_account(self, remember, id, currency):
+        if remember:
+            self.__data['other_users'][self.__id] = {
+                "theme": self.__theme,
+                "currency": self.__currency,
             }
-            save_preferences(filepath, user_id, data['users'][user_id])
-        return data['users'][user_id]
-    
-def save_preferences(filepath, user_id, preferences):
-    with open(filepath, 'r') as file:
-        data = json.load(file)
-    data['users'][user_id] = preferences
-    with open(filepath, 'w') as file:
-        json.dump(data, file, indent=4)
 
-
-def update_preferences(filepath, user_id, key, value):
-    preferences = load_preferences(filepath, user_id)
-    preferences[key] = value
-    save_preferences(filepath, user_id, preferences)
+        self.__id = id
+        self.__theme = "light"
+        self.__currency = currency
