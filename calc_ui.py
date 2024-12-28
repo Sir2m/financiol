ac1841c1eb25ab8e90493b0b679a5fa73e947ea1
Hossam Eldin Mahmod <hossammahmod555@gmail.com>
2024-12-28 21:12:40 +0200

    change calc to gride method

--- calc_ui.py
+++ calc_ui.py
@@ -44,42 +44,42 @@ def do_currency_exchange():
 
 # open sales
 def open_sale():
-    calc_frame.pack_forget()
-    sale_frame.pack(pady=20 ,padx=30)
+    calc_frame.grid_forget()
+    sale_frame.grid(row=1, column=0, pady=20, padx=30)
 
 # open taxs
 def open_tax():
-    calc_frame.pack_forget()
-    tax_frame.pack(pady=20 ,padx=30)
+    calc_frame.grid_forget()
+    tax_frame.grid(row=1, column=0, pady=20, padx=30)
 
 # open interest
 def open_interest():
-    calc_frame.pack_forget()
-    interest_frame.pack(pady=20 ,padx=30)    
+    calc_frame.grid_forget()
+    interest_frame.grid(row=1, column=0, pady=20, padx=30)    
 
 # open currency exchange
 def open_exchange():
-    calc_frame.pack_forget()
-    currency_frame.pack(pady=20 ,padx=30)
+    calc_frame.grid_forget()
+    currency_frame.grid(row=1, column=0, pady=20, padx=30)
 
 # back to calc from sale
 def back_from_sale():
-    sale_frame.pack_forget()
-    calc_frame.pack(pady=20 ,padx=30)
+    sale_frame.grid_forget()
+    calc_frame.grid(row=1, column=0, pady=20, padx=30)
 
 def back_from_tax():
-    tax_frame.pack_forget()
-    calc_frame.pack(pady=20 ,padx=30)    
+    tax_frame.grid_forget()
+    calc_frame.grid(row=1, column=0, pady=20, padx=30)    
 
 # back to calc from interest
 def back_from_inter():
-    interest_frame.pack_forget()
-    calc_frame.pack(pady=20 ,padx=30)
+    interest_frame.grid_forget()
+    calc_frame.grid(row=1, column=0, pady=20, padx=30)
 
 # back to calc from currency exchange
 def back_from_curr():
-    currency_frame.pack_forget()
-    calc_frame.pack(pady=20 ,padx=30)    
+    currency_frame.grid_forget()
+    calc_frame.grid(row=1, column=0, pady=20, padx=30)    
 
 # Initialize the main window
 app = ctk.CTk()
@@ -88,43 +88,42 @@ app.title("Financial Calculations")
 
 # calculations frame
 calc_frame = ctk.CTkFrame(master=app)
-calc_frame.pack(pady=20, padx=30)
+calc_frame.grid(row=1, column=0, pady=20, padx=30)
 
 sale_button = ctk.CTkButton(master=calc_frame,text="sales calc" , command=open_sale)
-sale_button.pack(pady=10 , padx=20)
+sale_button.grid(row=0, column=0, pady=10, padx=20)
  
 tax_button = ctk.CTkButton(master=calc_frame,text="taxs calc" , command=open_tax)
-tax_button.pack(pady=10 , padx=20)
+tax_button.grid(row=1, column=0, pady=10, padx=20)
 
 inter_button =  ctk.CTkButton(master=calc_frame,text="interest calc" , command=open_interest)
-inter_button.pack(pady=10 , padx=20)
+inter_button.grid(row=2, column=0, pady=10, padx=20)
 
 curr_button = ctk.CTkButton(master=calc_frame,text="currency exchange" , command=open_exchange)
-curr_button.pack(pady=10 , padx=20)
+curr_button.grid(row=3, column=0, pady=10, padx=20)
 
  
 # Frame for sale Calculation
 sale_frame = ctk.CTkFrame(master=app)
 
 sale_label = ctk.CTkLabel(master=sale_frame, text="sales Calculation", font=("Arial", 16))
-sale_label.pack(pady=10)
+sale_label.grid(row=0, column=0, pady=10)
 
 sale_value_entry = ctk.CTkEntry(master=sale_frame,placeholder_text="enter value")
-sale_value_entry.pack(pady=5)
+sale_value_entry.grid(row=1, column=0, pady=5)
 
 s_percent_entry = ctk.CTkEntry(master=sale_frame, placeholder_text="percent")
-s_percent_entry.pack(pady=5)
-
+s_percent_entry.grid(row=2, column=0, pady=5)
 
 s_change_var = ctk.IntVar()
 s_change_check = ctk.CTkCheckBox(master=sale_frame, text="return change", variable=s_change_var)
-s_change_check.pack(pady=5)
+s_change_check.grid(row=3, column=0, pady=5)
 
 sale_button = ctk.CTkButton(master=sale_frame, text="Calculate sale", command=do_sale)
-sale_button.pack(pady=10)
+sale_button.grid(row=4, column=0, pady=10)
 
-back_botton = ctk.CTkButton(master=sale_frame ,text="Back",command=back_from_sale)
-back_botton.pack(pady=5)
+back_button = ctk.CTkButton(master=sale_frame ,text="Back",command=back_from_sale)
+back_button.grid(row=5, column=0, pady=5)
 
 
 
@@ -132,23 +131,23 @@ back_botton.pack(pady=5)
 tax_frame = ctk.CTkFrame(master=app)
 
 tax_label = ctk.CTkLabel(master=tax_frame, text="Tax Calculation", font=("Arial", 16))
-tax_label.pack(pady=10)
+tax_label.grid(row=0, column=0, pady=10)
 
 tax_value_entry = ctk.CTkEntry(master=tax_frame,placeholder_text="enter value")
-tax_value_entry.pack(pady=5)
+tax_value_entry.grid(row=1, column=0, pady=5)
 
 t_percent_entry = ctk.CTkEntry(master=tax_frame, placeholder_text="percent")
-t_percent_entry.pack(pady=5)
+t_percent_entry.grid(row=2, column=0, pady=5)
 
 t_change_var = ctk.IntVar()
 t_change_check = ctk.CTkCheckBox(master=tax_frame,text="return change", variable=t_change_var)
-t_change_check.pack(pady=5)
+t_change_check.grid(row=3, column=0, pady=5)
 
 tax_button = ctk.CTkButton(master=tax_frame, text="Calculate Tax", command=do_Tax)
-tax_button.pack(pady=10)
+tax_button.grid(row=4, column=0, pady=10)
 
-back_botton = ctk.CTkButton(master=tax_frame ,text="Back",command=back_from_tax)
-back_botton.pack(pady=5)
+back_button = ctk.CTkButton(master=tax_frame ,text="Back",command=back_from_tax)
+back_button.grid(row=5, column=0, pady=5)
 
 
 
@@ -156,26 +155,26 @@ back_botton.pack(pady=5)
 interest_frame = ctk.CTkFrame(master=app)
 
 interest_label = ctk.CTkLabel(master=interest_frame, text="Interest Calculation", font=("Arial", 16))
-interest_label.pack(pady=10)
+interest_label.grid(row=0, column=0, pady=10)
 
 principal_entry = ctk.CTkEntry(master=interest_frame,placeholder_text="principal")
-principal_entry.pack(pady=5)
+principal_entry.grid(row=1, column=0, pady=5)
 
 rate_entry = ctk.CTkEntry(master=interest_frame,placeholder_text="Rate")
-rate_entry.pack(pady=5)
+rate_entry.grid(row=2, column=0, pady=5)
 
 period_entry = ctk.CTkEntry(master=interest_frame,placeholder_text="period")
-period_entry.pack(pady=5)
+period_entry.grid(row=3, column=0, pady=5)
 
 installments_var = ctk.IntVar()
 installments_check = ctk.CTkCheckBox(master=interest_frame, text="Installments", variable=installments_var)
-installments_check.pack(pady=5)
+installments_check.grid(row=4, column=0, pady=5)
 
 interest_button = ctk.CTkButton(master=interest_frame, text="Calculate Interest", command=do_calc_interest)
-interest_button.pack(pady=10)
+interest_button.grid(row=5, column=0, pady=10)
 
-back_botton = ctk.CTkButton(master=interest_frame ,text="Back",command=back_from_inter)
-back_botton.pack(pady=5)
+back_button = ctk.CTkButton(master=interest_frame ,text="Back",command=back_from_inter)
+back_button.grid(row=6, column=0, pady=5)
 
 
 
@@ -183,26 +182,25 @@ back_botton.pack(pady=5)
 currency_frame = ctk.CTkFrame(master=app)
 
 currency_label = ctk.CTkLabel(master=currency_frame, text="Currency Exchange", font=("Arial", 16))
-currency_label.pack(pady=10)
+currency_label.grid(row=0, column=0, pady=10)
 
 from_currency_label = ctk.CTkLabel(master=currency_frame, text="From Currency")
-from_currency_label.pack(pady=5)
+from_currency_label.grid(row=1, column=0, pady=5)
 from_currency_menu = ctk.CTkOptionMenu(master=currency_frame,values=curr_list)
-from_currency_menu.pack(pady=5)
+from_currency_menu.grid(row=2, column=0, pady=5)
 
 to_currency_label = ctk.CTkLabel(master=currency_frame, text="To Currency")
-to_currency_label.pack(pady=5)
+to_currency_label.grid(row=3, column=0, pady=5)
 to_currency_menu = ctk.CTkOptionMenu(master=currency_frame,values=curr_list)
-to_currency_menu.pack(pady=5)
+to_currency_menu.grid(row=4, column=0, pady=5)
 
 amount_entry = ctk.CTkEntry(master=currency_frame,placeholder_text="Amount")
-amount_entry.pack(pady=5)
+amount_entry.grid(row=5, column=0, pady=5)
 
 currency_button = ctk.CTkButton(master=currency_frame, text="Exchange Currency", command=do_currency_exchange)
-currency_button.pack(pady=10)
+currency_button.grid(row=6, column=0, pady=5)
 
-back_botton = ctk.CTkButton(master=currency_frame ,text="Back",command=back_from_curr)
-back_botton.pack(pady=5)
+back_button = ctk.CTkButton(master=currency_frame ,text="Back",command=back_from_curr)
+back_button.grid(row=7, column=0, pady=5)
 
-# Start the main loop
 app.mainloop()
